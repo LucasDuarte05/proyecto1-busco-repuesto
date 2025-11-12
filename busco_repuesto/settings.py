b2f15ff115b7f9e6a32489a9144d11fb793d9b93
LucasDuarte05 <lucasduartee.05@gmail.com>
2025-11-12 12:25:45 -0300

    estticos

--- busco_repuesto/settings.py
+++ busco_repuesto/settings.py
@@ -124,11 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # si usás una carpeta 'static' en el proyecto
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # donde collectstatic guarda los archivos
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # login normal
